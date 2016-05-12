039fc50f5c882bc0ba4314eddec0461c255afd7f
Kevin Rankine <krankine@holyseasgpu08.rc.fas.harvard.edu>
2016-05-11 21:24:22 -0400

    fixed issue

--- mean_body.sh
+++ mean_body.sh
@@ -2,7 +2,7 @@
 
 #SBATCH --mail-type=END
 #SBATCH --mail-user=rankine@college.harvard.edu
-#SBATCH -t 0-01:30
+#SBATCH -t 0-06:00
 #SBATCH --job-name=MEAN_BODY
 #SBATCH --output=logs/mean_body.log
 #SBATCH --error=logs/fuck.fuck
@@ -12,5 +12,5 @@
 #SBATCH --nodes=1
 #SBATCH --tasks-per-node=1
 
-th main.lua -model rnn -cuda 1 -eta 1e-3 -d_hid 280 -nepochs 5 -margin 0.02 -nbatches 8 -dropout 0.1 -pool mean -to_file gru.dat -body 1
+th main.lua -model rnn -cuda 1 -eta 1e-3 -d_hid 280 -nepochs 15 -margin 0.02 -nbatches 8 -dropout 0.1 -pool mean -to_file gru.dat -body 1
 
